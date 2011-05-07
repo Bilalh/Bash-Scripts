87b537aebe4a7d848d9c37bf192418b0cd61de13
Bilal Hussain <bhterra@gmail.com>
2011-05-07 23:32:16 +0100

    mac

---
+++ mac.sh
@@ -0,0 +1,18 @@
+function ql () {
+	#Display the Quick Look preview for the given file.
+	if [ -z "$1" ] ; then
+	echo "Usage: ql "
+	exit 1
+	fi
+	 qlmanage -p $1 >& /dev/null &
+}
+
+alias qlr="qlmanage -r"
+
+
+function dict () {
+	 open dict:///"$@" ; 
+}
+
+alias hda="hdiutil attach"
+alias hdd="hdiutil detach"
