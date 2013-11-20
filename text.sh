92f0e772204ff63fc4bb120cdb2c226febb52895
Bilal Syed Hussain <bilalshussain@gmail.com>
2013-11-20 20:28:13 +0000

    create a new file based on a predefined template

--- text.sh
+++ text.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 function letter_frequencies(){
 	cat "$1" | 
-	    awk -vFS="" '{for(i=1;i<=NF;i++){ if($i~/[a-zA-Z]/) { w[tolower($i)]++} } }END{for(i in w) print i,w[i]}' | 
-	    sort |
-	    cut -c 3- |
-	    spark
+		awk -vFS="" '{for(i=1;i<=NF;i++){ if($i~/[a-zA-Z]/) { w[tolower($i)]++} } }END{for(i in w) print i,w[i]}' | 
+		sort |
+		cut -c 3- |
+		spark
 }
 
 function ss(){
@@ -15,5 +15,21 @@ function ss(){
 }
 
 function count_source(){
-  wc `ack --ignore-dir=dist -f $@` 2> /dev/null	
+  wc `ack --ignore-dir=dist -f $@` 2> /dev/null 
+}
+
+
+function matee() {
+	local template_dir="/Users/bilalh/Library/Application Support/com.trankynam.XtraFinder/NewFileTemplates"
+	 if [  $# -eq 1	 ] ; then
+		 ext="${1##*.}"
+		 case "$ext" in
+			 applescript|eprime|essence|html|param|py|rb|rtf|sh|solution)  
+			 	cat "${template_dir}/Untitled.${ext}" | mate --line 10 --name "$1" ;;
+				
+			 *) mate "$@" ;;
+		 esac
+	else
+		mate "$@"
+	fi
 }
