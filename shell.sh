6237bbbe97af470d0a69a0c31b21e1e2f57bd855
Bilal Syed Hussain <bilalshussain@gmail.com>
2013-11-20 20:31:15 +0000

    ipython functions. nicer $PATH and record function  to tee all output of a command along with the command the made the output

--- shell.sh
+++ shell.sh
@@ -5,6 +5,34 @@ alias reset_split='unset IFS'
 alias reload=". ${HOME}/.bashrc"
 fi
 
+function qpython(){
+	export PYTHONPATH=/usr/local/lib/python2.7/site-packages:$PYTHONPATH 
+	nohup /usr/local/bin/ipython qtconsole --matplotlib --colors=linux $* & 
+	unset PYTHONPATH	
+}
+
+function qpython3(){
+	(export PATH=/Users/bilalh/.local/anaconda/bin:$PATH
+	export PYTHONPATH=/Users/bilalh/.local/anaconda/lib/python3.3/site-packages:$PYTHONPATH
+	source activate py3
+	ipython qtconsole --matplotlib --colors=linux $* &)
+}
+
+function jpython3(){
+	export PATH=/Users/bilalh/.local/anaconda/bin:$PATH
+	export PYTHONPATH=/Users/bilalh/.local/anaconda/lib/python3.3/site-packages:$PYTHONPATH
+	source activate py3
+	ipython $*  --colors=linux
+
+}
+
+
+function jpython(){
+	(PYTHONPATH=/usr/local/lib/python2.7/site-packages:$PYTHONPATH; 
+	 /usr/local/bin/ipython --colors=linux $*;
+    )
+}
+
 function spacesToUnderscore () {
 	find . -name '* *' | while read file;
 	do
@@ -43,4 +71,28 @@ whichv(){
 rnd(){
 	num=${1:-50}
 	$[($RANDOM %  $num) +1]
+}
+
+
+function npath(){
+	echo "${PATH//:/$'\n'}"
+}
+
+function teeDate() {
+	tee "$@-`date +%F_%H-%M_%s`"
+}
+
+function record(){
+    if [[ -z "${1}" || -z "${2}" ]]; then
+	    echo "$0 name  command"
+    else
+	    name="$1"
+	    shift
+	    if [ "`basename \"${name}\"`" != "${name}" ]; then
+		    mkdir -p "`dirname \"${name}\"`"
+		fi
+	    fp="${name}-`date +%F_%H-%M_%s`"
+	    echo "$@" > "${fp}.cmds"
+	    $@ 2>&1 | tee "${fp}.log"
+    fi
 }
